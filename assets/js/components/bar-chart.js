578d8e21ae8451189c5afc13e86289d3687fdfa9
fumikito <guy@hametuha.com>
2017-12-12 01:58:12 +0900

    Add assets for packagist

---
+++ assets/js/components/bar-chart.js
@@ -0,0 +1,2 @@
+!function(t){"use strict";Vue.component("hb-bar-chart",{"extends":VueChartJs.Bar,mixins:[VueChartJs.mixins.reactiveProp],props:["chartData","options"],mounted:function(){this.renderChart(this.chartData,this.options)}})}(jQuery);
+//# sourceMappingURL=../map/components/bar-chart.js.map
---
+++ assets/js/components/line-chart.js
@@ -0,0 +1,2 @@
+!function(t){"use strict";Vue.component("hb-line-chart",{"extends":VueChartJs.Line,mixins:[VueChartJs.mixins.reactiveProp],props:["chartData","options"],mounted:function(){this.renderChart(this.chartData,this.options)}})}(jQuery);
+//# sourceMappingURL=../map/components/line-chart.js.map
