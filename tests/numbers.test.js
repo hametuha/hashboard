01e34424be7d38a2d9a40723e0cc176ef0df3615
fumikito <guy@hametuha.com>
2020-01-06 18:43:21 +0900

    #6 Restore .babelrc for Jest.

--- tests/numbers.test.js
+++ tests/numbers.test.js
@@ -1,18 +1,38 @@
 import Vue from 'vue';
 import { numberFormat, moneyFormat } from '../src/js/filters/numbers';
 
-test('Check number format', () => {
-  expect(numberFormat(1000)).toBe('1,000');
-  expect(numberFormat(1000000)).toBe('1,000,000');
-  expect(numberFormat(100)).toBe('100');
-  expect(numberFormat(1000.102)).toBe('1,000.102');
-  expect(numberFormat(1000.102, false)).toBe('1,000');
-  expect(numberFormat(10000000000.1)).toBe('10,000,000,000.1');
-});
+describe( 'Check number format', () => {
+	test( 'Thousnad', () => {
+		expect( numberFormat( 1000 ) ).toBe( '1,000' );
+	} );
+	test( 'Million', () => {
+		expect( numberFormat( 1000000 ) ).toBe( '1,000,000' );
+	} );
+	test( 'Hundred', () => {
+		expect( numberFormat( 100 ) ).toBe( '100' );
+	} );
+	test( 'Float', () => {
+		expect( numberFormat( 1000.102 ) ).toBe( '1,000.102' );
+	} );
+	test( 'Floor', () => {
+		expect( numberFormat( 1000.102, false ) ).toBe( '1,000' );
+	} );
+	test( 'Big number', () => {
+		expect( numberFormat( 10000000000.1 ) ).toBe( '10,000,000,000.1' );
+	} );
+} );
 
-test('Check Money format', () => {
-  expect(moneyFormat(1000, 'jpy')).toBe('¥1,000');
-  expect(moneyFormat(1000000)).toBe('$1,000,000');
-  expect(moneyFormat('100.00', 'eur')).toBe('€100.00');
-  expect(moneyFormat(1000000, 'usd', false)).toBe('1,000,000');
-});
+describe( 'Check Money format', () => {
+	test( 'JPY', () => {
+		expect( moneyFormat( 1000, 'jpy' ) ).toBe( '¥1,000' );
+	} );
+	test( 'USD', () => {
+		expect( moneyFormat( 1000000 ) ).toBe( '$1,000,000' );
+	} );
+	test( 'Euro', () => {
+		expect( moneyFormat( '100.00', 'eur' ) ).toBe( '€100.00' );
+	} );
+	test( 'No prefix.', () => {
+		expect( moneyFormat( 1000000, 'usd', false ) ).toBe( '1,000,000' );
+	} );
+} );
