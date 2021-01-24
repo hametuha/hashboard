b5644305eca0174c0912962b4fdf1a13600d7733
fumikito <guy@hametuha.com>
2021-01-25 02:50:15 +0900

    Update pattern library

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -7,9 +7,12 @@ rm -rf .git
 rm -rf .github
 rm -rf .gitignore
 rm -rf .babelrc
+rm -rf .browserslistrc
 rm -rf .eslintrc
 rm -rf bin
 rm -rf node_modules
 rm -rf tests
 rm -rf phpunit.xml.dist
 rm -rf vendor
+rm -rf stylelint.config.js
+rm -rf webpack.config.js
